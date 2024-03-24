fa996b41b1d4cb9c40bca24d3d2709f7ac34c450
vashika21 <vashika0221@gmail.com>
2024-03-24 15:45:16 +0530

    updated

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const httpMocks = require("node-mocks-http");
 const exampleRouteHandler = require("./example-router");
 describe("Example Test 1", () => {
-  it("should return 'Good bye !!!II  ' for GET /example", () => {
+  it("should return 'Good night everyone !!!II  ' for GET /example", () => {
     const mockRequest = httpMocks.createRequest({
       method: "GET",
       url: "/example"
@@ -10,7 +10,7 @@ describe("Example Test 1", () => {
     const mockResponse = httpMocks.createResponse();
     exampleRouteHandler(mockRequest, mockResponse);
     const actualResponseBody = mockResponse._getData();
-    const expectedResponseBody = "Hello Earthling!!!!";
+    const expectedResponseBody = "Goodbye Earthling!!!!";
     assert(actualResponseBody, expectedResponseBody);
   });
 });
